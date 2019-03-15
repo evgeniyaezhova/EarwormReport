f153571bf6634bd8e4d6ddbd02f12302b3b67b64
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-03-15 15:39:31 -0400

    user queries and routes complete

---
+++ backend/db/queries/userQueries.js
@@ -0,0 +1,67 @@
+const { db } = require('../index');
+
+const getAllUsers = (req, res, next) => {
+  db.any("SELECT * FROM users")
+    .then(users => {
+      res.status(200).json({
+        status: "success!",
+        users: users,
+        message: "got all users!"
+      });
+    })
+    .catch(err => {
+      return next(err)
+    });
+};
+
+const getSingleUser = (req, res, next) => {
+  let userId = parseInt(req.params.id);
+  db.one('SELECT * FROM users WHERE id=$1', [userId])
+  .then(user => {
+    res.status(200)
+    .json({
+      status: "success",
+      user: user,
+      message: "got one user"
+    });
+  })
+  .catch(err => {
+    return next(err)
+  })
+};
+
+const createNewUser = (req, res, next) => {
+  db.none(
+  'INSERT INTO users (username) VALUES (${username})',
+  req.body)
+    .then(() => {
+      res.status(200).json({
+        message: "created a new user"
+      });
+    })
+    .catch(err => {
+      next(err);
+    })
+};
+
+const deleteSingleUser = (req, res, next) => {
+  let userId = parseInt(req.params.id);
+  db.result('DELETE FROM users WHERE id=$1', userId)
+  .then(result => {
+    res.status(200)
+    .json({
+      status: "success",
+      message: "deleted a single user"
+    });
+  })
+  .catch(err => {
+    return next(err)
+  })
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+  deleteSingleUser
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {getAllUsers, getSingleUser, createNewUser, deleteSingleUser} = require('../db/queries/userQueries.js')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', getAllUsers);
+router.get('/:id', getSingleUser);
+router.post('/', createNewUser);
+router.delete('/:id', deleteSingleUser);
+
 
 module.exports = router;
